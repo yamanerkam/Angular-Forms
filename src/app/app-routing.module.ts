715ccbb75c9cd6e79f56756e30b9d239b4980237
yamanerkam <erkamyaman35@gmail.com>
2024-09-15 15:09:41 +0300

    routing-and-moduling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ModelDrivenFormComponent } from './components/model-driven-form/model-driven-form.component';
+import { TemplateDrivenFormComponent } from './components/template-driven-form/template-driven-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "model-driven", component: ModelDrivenFormComponent },
+  { path: "template-driven", component: TemplateDrivenFormComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TemplateDrivenFormComponent } from './components/template-driven-form/template-driven-form.component';
-import { ModelDrivenFormComponent } from './components/model-driven-form/model-driven-form.component';
+import { TemplateDrivenFormModule } from './components/template-driven-form/template-driven-form.module';
+import { ModelDrivenFormModule } from './components/model-driven-form/model-driven-form.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateDrivenFormComponent,
-    ModelDrivenFormComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TemplateDrivenFormModule,
+    ModelDrivenFormModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [TemplateDrivenFormModule,
+    ModelDrivenFormModule]
 })
 export class AppModule { }
