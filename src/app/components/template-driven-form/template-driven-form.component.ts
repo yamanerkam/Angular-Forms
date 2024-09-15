ac49fee5bc06999287417826641cba5ea1464afe
yamanerkam <erkamyaman35@gmail.com>
2024-09-15 15:55:38 +0300

    viewChild

--- src/app/components/template-driven-form/template-driven-form.component.ts
+++ src/app/components/template-driven-form/template-driven-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-driven-form',
@@ -6,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./template-driven-form.component.css']
 })
 export class TemplateDrivenFormComponent implements OnInit {
+  @ViewChild("frm", { static: false }) frm: NgForm | undefined;
 
-  submit(data: any) {
+  submit(data: any, form: any) {
     console.log(data);
+    console.log(this.frm?.value)
   }
   constructor() { }
 
